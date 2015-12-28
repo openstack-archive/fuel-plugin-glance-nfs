2ffe12ce8da335ea74d5352006e48bcb3b60a3fa
Lnic <loic.nicolle@orange.com>
2015-12-28 09:41:07 +0100

    Solve problem with vmware option
    Change-Id: I07fd6da29425671664b39f2aec776caeddf3c406

--- deployment_scripts/puppet/delete.pp
+++ deployment_scripts/puppet/delete.pp
@@ -1,4 +1,9 @@
   # have to find a better way to do this, if we can execute plugin before upload_cirros task we don't need this anymore
-  exec{ "image-delete":
-  command       => "/bin/bash -c 'source /root/openrc && /usr/bin/glance image-delete TestVM'",
-  }
+      glance_image { "TestVM":
+        ensure          => 'absent',
+        name            => "TestVM",
+      }
+      glance_image { "TestVM-VMDK":
+        ensure          => 'absent',
+        name            => "TestVM-VMDK",
+      }
